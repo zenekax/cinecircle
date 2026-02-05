b0e3ff6893a90a3c11ac9c4eda8cdf0332e97efc
zenekax <nachito.basso@gmail.com>
2026-02-05 00:20:31 -0300

    Add PWA support - app is now installable

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,65 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
+      manifest: {
+        name: 'CineCircle - Recomendaciones',
+        short_name: 'CineCircle',
+        description: 'Comparte y descubre películas y series con tus amigos',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#0D0D0D',
+        theme_color: '#3ECF8E',
+        orientation: 'portrait-primary',
+        categories: ['entertainment', 'social'],
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/api\.themoviedb\.org\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'tmdb-api-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 // 24 horas
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/image\.tmdb\.org\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'tmdb-images-cache',
+              expiration: {
+                maxEntries: 200,
+                maxAgeSeconds: 60 * 60 * 24 * 7 // 7 días
+              }
+            }
+          }
+        ]
+      }
+    })
+  ],
 })
